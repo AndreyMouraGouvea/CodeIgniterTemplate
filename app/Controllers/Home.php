0a23e5fed0c7aff6a073fe33a2add700b944d72d
AndreyMouraGouvea <andreygouveia.05@gmail.com>
2022-03-09 14:03:33 -0300

    template >> routes

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,17 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('home');
+        // return view('home');
+        echo view('template/header');
+        echo view('home');
+        echo view('template/footer');
+    }
+
+    public function page($page='home') {
+
+        echo view('template/header');
+        echo view('home');
+        echo view('template/footer');
+
     }
 }
---
+++ app/Views/template/footer.php
@@ -0,0 +1,5 @@
+<div class="row">
+    <h1>Footer</h1>
+</div>
+</body>
+</html>
