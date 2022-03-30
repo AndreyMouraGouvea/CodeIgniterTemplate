781bedf2d5fe5274dea12c731d1badcb6c9d6e53
AndreyMouraGouvea <andreygouveia.05@gmail.com>
2022-03-30 15:01:58 -0300

    update Model

---
+++ app/Models/PessoasModel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models; 
+
+use CodeIgniter\Model;
+
+class PessoasModel extends Model{
+    
+    protected $table = 'tb_pessoas';
+    protected $primaryKey = 'id';
+    protected $useAutoIncrement = true;
+    
+    protected $allowedFields = ['nome', 'profissao', 'idade'];
+
+    public function getPessoas() {
+
+        return $this->findAll();
+
+    }
+
+}
